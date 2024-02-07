697e708b8d32cae96ea8f59039c67b533be5f906
Michal Dyczkowski <themich4@gmail.com>
2024-02-07 20:05:37 +0100

    Remove console.log statements in +layout.ts

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,9 +3,7 @@ import { browser } from '$app/environment';
 import { PUBLIC_POSTHOG_KEY } from '$env/static/public';
 
 export const load = async () => {
-	console.log('111');
 	if (browser) {
-		console.log('222');
 		posthog.init(PUBLIC_POSTHOG_KEY, {
 			api_host: 'https://app.posthog.com'
 		});
